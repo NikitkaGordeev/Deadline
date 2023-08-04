08b61a01aaed2690d390f8c9686d6359942551b0
Nikita G <jobnikitka152rus@gmail.com>
2023-08-04 23:26:38 +0300

    add all

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,4 +1,40 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
 public class DataHelper {
+
+    private DataHelper() {
+    }
+    public static AuthUser getAuthUser() {
+        return new AuthUser("vasya", "qwerty123");
+    }
+    private static Faker faker = new Faker(new Locale("en"));
+    @Value
+    public static class AuthUser{
+        String login;
+        String password;
+    }
+    @Value
+    public static class VerifyCode{
+        String code;
+    }
+
+    private static String getRandomLogin(){
+        return faker.name().username();
+    }
+    private static String getRandomPass(){
+        return faker.internet().password();
+    }
+    public static AuthUser getRandomUser(){
+        return new AuthUser(getRandomLogin(),getRandomPass());
+    }
+
+    public static VerifyCode getRandomVerifyCode(){
+        return new VerifyCode(faker.numerify("111111"));
+    }
+
 }
--- src/test/java/ru/netology/tests/DeadlineTests.java
+++ src/test/java/ru/netology/tests/DeadlineTests.java
@@ -1,3 +1,89 @@
 package ru.netology.tests;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.pages.LoginPage;
+
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDB;
+
+
 public class DeadlineTests {
+
+    @AfterAll
+    static void cleaner() {
+        cleanDB();
+    }
+
+    @Test
+    void SuccessLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authUser = DataHelper.getAuthUser();
+        var verifyPage = loginPage.validLogin(authUser);
+        verifyPage.verifyPageVisibility();
+        var verifyCode = SQLHelper.getVerifyCode();
+        verifyPage.validVerify(verifyCode.getCode());
+    }
+
+    @Test
+    void RandomUser() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+    @Test
+    void InvalidLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authUser = new DataHelper.AuthUser(DataHelper.getRandomUser().getLogin(),
+                DataHelper.getAuthUser().getPassword());
+        loginPage.validLogin(authUser);
+        loginPage.getError();
+    }
+
+    @Test
+    void InvalidPass() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authUser = new DataHelper.AuthUser(DataHelper.getAuthUser().getLogin(),
+                DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authUser);
+        loginPage.getError();
+    }
+
+    @Test
+    void InvalidVerifyCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authUser = DataHelper.getAuthUser();
+        var verifyPage = loginPage.validLogin(authUser);
+        verifyPage.verifyPageVisibility();
+        var verifyCode = DataHelper.getRandomVerifyCode().getCode();
+        verifyPage.verify(verifyCode);
+        verifyPage.getError();
+    }
+
+    @Test
+    void PassThreeTimes() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authUserOne = new DataHelper.AuthUser(DataHelper.getAuthUser().getLogin(),
+                DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authUserOne);
+        loginPage.getError();
+        loginPage.clean();
+        clearBrowserCookies();
+        var authUserTwo = new DataHelper.AuthUser(DataHelper.getAuthUser().getLogin(),
+                DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authUserTwo);
+        loginPage.getError();
+        loginPage.clean();
+        clearBrowserCookies();
+        var authUserThree = new DataHelper.AuthUser(DataHelper.getAuthUser().getLogin(),
+                DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authUserThree);
+        loginPage.getBlock();
+    }
 }
