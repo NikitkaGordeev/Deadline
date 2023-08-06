23f097c6ea0f4a667fc1d028a03ece6521be28e5
Nikita <130284238+NikitkaGordeev@users.noreply.github.com>
2023-08-06 10:02:26 +0300

    Update SQLHelper.java

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -23,9 +23,9 @@ public class SQLHelper {
     @SneakyThrows
     public static DataHelper.VerifyCode getVerifyCode() {
         var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
-        var connection = getConnection();{
-            var result = runner.query(connection, codeSQL, new ScalarHandler<String>());
-            return new DataHelper.VerifyCode(result);
+        var connection = getConnection();
+        var result = runner.query(connection, codeSQL, new ScalarHandler<String>());
+        return new DataHelper.VerifyCode(result);
         }
     }
 
